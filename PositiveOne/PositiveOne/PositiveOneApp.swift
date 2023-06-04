774eba094220d9952048f5da67c768e5b7c28832
youngsun <dudtjs1021@hansung.ac.kr>
2023-06-04 14:46:58 +0900

    💄 [style] ColorSet 추가

---
+++ PositiveOne/PositiveOne/PositiveOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PositiveOneApp.swift
+//  PositiveOne
+//
+//  Created by 임영선 on 2023/06/04.
+//
+
+import SwiftUI
+
+@main
+struct PositiveOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PositiveOne/PositiveOne/Resources/Extensions/Extension+Color.swift
@@ -0,0 +1,31 @@
+//
+//  Extension+Color.swift
+//  PositiveOne
+//
+//  Created by 임영선 on 2023/06/04.
+//
+
+import SwiftUI
+
+extension Color {
+    enum Custom {
+        static let YellowShadow50 = Color("YellowShadow50")
+        static let YellowShadow = Color("YellowShadow")
+        static let Yellow80 = Color("Yellow80")
+        static let Yellow70 = Color("Yellow70")
+        static let Yellow60 = Color("Yellow60")
+        static let TitleColor = Color("TitleColor")
+        static let Red = Color("Red")
+        static let PositiveYellowStroke = Color("PositiveYellowStroke")
+        static let PositiveYellow = Color("PositiveYellow")
+        static let MovedOnGreenStroke = Color("MovedOnGreenStroke")
+        static let Black10 = Color("Black10")
+        static let Black20 = Color("Black20")
+        static let Black30 = Color("Black30")
+        static let Black40 = Color("Black40")
+        static let Black60 = Color("Black60")
+        static let Black70 = Color("Black70")
+        static let Black90 = Color("Black90")
+        static let Black100 = Color("Black100")
+    }
+}
