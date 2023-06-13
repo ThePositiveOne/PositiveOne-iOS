c08b66f26f8067f5324848ee4409bb0320045797
youngsun <dudtjs1021@hansung.ac.kr>
2023-06-13 23:07:51 +0900

    ✨ [feat] 설정 네비바 타이틀 추가

--- PositiveOne/PositiveOne/Settings/SettingsView.swift
+++ PositiveOne/PositiveOne/Settings/SettingsView.swift
@@ -10,41 +10,34 @@ import SwiftUI
 struct SettingsView: View {
     
     init() {
-        let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.configureWithOpaqueBackground()
-        navBarAppearance.backgroundColor = UIColor(Color.white)
-        navBarAppearance.titleTextAttributes = [.font : UIFont(name: "GangwonEduPowerExtraBold", size: 20)!]
-        navBarAppearance.shadowColor = .clear
-        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
-        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        
     }
     
     var body: some View {
-        NavigationView {
-            List {
-                Text("로그인")
-                    .foregroundColor(.Custom.Black70)
-                    .font(CustomFont.PretendardMedium(size: 16).font)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: -20))
-                Text("이용약관")
-                    .foregroundColor(.Custom.Black70)
-                    .font(CustomFont.PretendardMedium(size: 16).font)
-                Text("개인정보 정책")
-                    .foregroundColor(.Custom.Black70)
-                    .font(CustomFont.PretendardMedium(size: 16).font)
-            }
-            .listRowInsets(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 30))
-            .listStyle(PlainListStyle())
-            
-            .toolbar(.hidden, for: .tabBar)
-            .scrollContentBackground(.hidden)
-            .background(.white)
-            .frame(width: UIScreen.main.bounds.width-40)
-            .padding(.leading, -20)
-            .navigationTitle("설정")
+        
+        List {
+            Text("로그인")
+                .foregroundColor(.Custom.Black70)
+                .font(CustomFont.PretendardMedium(size: 16).font)
+            Text("이용약관")
+                .foregroundColor(.Custom.Black70)
+                .font(CustomFont.PretendardMedium(size: 16).font)
+            Text("개인정보 정책")
+                .foregroundColor(.Custom.Black70)
+                .font(CustomFont.PretendardMedium(size: 16).font)
         }
+        .listStyle(PlainListStyle())
+        //.toolbar(.hidden, for: .tabBar)
+        .navigationTitle("설정")
+        .scrollContentBackground(.hidden)
+        .background(.white)
+        .frame(width: UIScreen.main.bounds.width-40)
+        .padding(.leading, -20)
+        .padding(.top, 20)
         
     }
+    
+    
 }
 
 struct SettingsView_Previews: PreviewProvider {
