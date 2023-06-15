107b8020e830af05dfc2c8200d000c8caa041107
youngsun <dudtjs1021@hansung.ac.kr>
2023-06-15 11:07:14 +0900

    ✨ [feat] PositiveOneType 추가

---
+++ PositiveOne/PositiveOne/Model/PositiveOneType.swift
@@ -0,0 +1,15 @@
+//
+//  PositiveOneType.swift
+//  PositiveOne
+//
+//  Created by 임영선 on 2023/06/15.
+//
+
+import Foundation
+
+enum PositiveOneType {
+    case pleasureOne
+    case movedOne
+    case excitingOne
+    case thanksOne
+}
