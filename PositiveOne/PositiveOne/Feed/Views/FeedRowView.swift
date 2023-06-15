f64ad2ac4709e331f76b86c799423b0f7d3933a4
youngsun <dudtjs1021@hansung.ac.kr>
2023-06-15 20:14:47 +0900

    ✨ [feat] 피드 더보기 버튼 UI 구현

--- PositiveOne/PositiveOne/Feed/Views/FeedRowView.swift
+++ PositiveOne/PositiveOne/Feed/Views/FeedRowView.swift
@@ -11,59 +11,69 @@ struct FeedRowView: View {
     let feedContent: FeedContent
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            HStack {
+            HStack(alignment: .bottom) {
                 Text(feedContent.name)
                     .font(CustomFont.PretendardBold(size: 16).font)
                     .foregroundColor(.Custom.Black100)
                     .padding(.top, 18)
                 
+                Spacer()
+                
                 Button {
                     
                 } label: {
-                    //Image(systemName: "dot.square")
+                    Image("moreVertical")
                 }
 
             }
+            .padding(.leading, 16)
+            .padding(.trailing, 14)
             
             
             Text(feedContent.date)
                 .font(CustomFont.PretendardRegular(size: 12).font)
                 .foregroundColor(.Custom.Black60)
                 .padding(.top, 4)
+                .padding(.horizontal, 16)
             
             Text(feedContent.text)
             .font(CustomFont.PretendardMedium(size: 14).font)
             .foregroundColor(.Custom.Black70)
             .padding(.top, 10)
+            .padding(.horizontal, 16)
             
-            Button(action: {
-                
-            }, label: {
-                Image(systemName: "heart.fill")
-                    .resizable()
-                    .foregroundColor(.white)
-                    .scaledToFit()
-                    .frame(width: 12, height: 11)
-                    .offset(CGSize(width: 2, height: 0))
+            
+            HStack {
+                Spacer()
+                Button(action: {
                     
+                }, label: {
+                    Image(systemName: "heart.fill")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .scaledToFit()
+                        .frame(width: 12, height: 11)
+                        .offset(CGSize(width: 2, height: 0))
+                        
+                        
+                    Text("15")
+                        .foregroundColor(.white)
+                        .font(CustomFont.PretendardRegular(size: 14).font)
+                        
                     
-                Text("15")
-                    .foregroundColor(.white)
-                    .font(CustomFont.PretendardRegular(size: 14).font)
-                    
-                
-            })
-            .frame(width: 60, height: 26)
-            .background(feedContent.likeCheck ?Color.Custom.PositiveYellow : Color.Custom.Black30)
-            .cornerRadius(3)
-            .padding(.top, 2)
-            .padding(.leading, UIScreen.main.bounds.width-40-16-60-15)
+                })
+                .frame(width: 60, height: 26)
+                .background(feedContent.likeCheck ?Color.Custom.PositiveYellow : Color.Custom.Black30)
+                .cornerRadius(3)
+                .padding(.top, 2)
+                .padding(.leading, UIScreen.main.bounds.width-40-16-60-15)
+            }
+            .padding(.trailing, 16)
             
             Spacer()
         }
         .frame(width: UIScreen.main.bounds.width-40, height: 166)
-        .background(.gray)
-        .padding(.leading, 15)
+        //.background(.green)
         .listRowSeparator(.hidden)
     }
         
