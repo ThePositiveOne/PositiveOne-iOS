03ed683fcb3f4331d117dab9918a14d818aed02c
youngsun <dudtjs1021@hansung.ac.kr>
2023-06-20 23:18:14 +0900

    ✨ [feat] 피드 정렬을 위해 SortingType 구현

---
+++ PositiveOne/PositiveOne/Feed/Model/SortingType.swift
@@ -0,0 +1,13 @@
+//
+//  SortingType.swift
+//  PositiveOne
+//
+//  Created by 임영선 on 2023/06/20.
+//
+
+import Foundation
+
+enum SortingType: String {
+    case popular = "popular"
+    case new = "new"
+}
