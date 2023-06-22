85c7b615c7dc04a980a6c69e3e15c2c469a43d9e
youngsun <dudtjs1021@hansung.ac.kr>
2023-06-22 16:13:45 +0900

    ✨ [feat] 로그아웃 기능 구현

--- PositiveOne/PositiveOne/Settings/SettingsView.swift
+++ PositiveOne/PositiveOne/Settings/SettingsView.swift
@@ -12,18 +12,27 @@ struct SettingsView: View {
     let isLogin = Keychain.loadToken() != nil
     let notloggedInSettingList = ["로그인", "이용약관", "개인정보 정책"]
     let loggedInSettingList = ["로그아웃", "회원탈퇴", "이용약관", "개인정보 정책"]
+    @State var isPresentedLoginView = false
     @Binding var isTabbarHidden: Bool
     
     var body: some View {
         
         List {
             ForEach(isLogin ? loggedInSettingList : notloggedInSettingList, id: \.self) { item in
-                Text(item)
-                    .foregroundColor(.Custom.Black70)
-                    .font(CustomFont.PretendardMedium(size: 16).font)
+                Button {
+                    switch item {
+                    case "로그아웃":
+                        Keychain.deleteToken()
+                        isPresentedLoginView.toggle()
+                    default:
+                        break
+                    }
+                } label: {
+                    Text(item)
+                        .foregroundColor(.Custom.Black70)
+                        .font(CustomFont.PretendardMedium(size: 16).font)
+                }
             }
-            
-            
         }
         .listStyle(PlainListStyle())
         .scrollContentBackground(.hidden)
@@ -31,6 +40,10 @@ struct SettingsView: View {
         .frame(width: UIScreen.main.bounds.width-40)
         .padding(.leading, -20)
         .padding(.top, 20)
+        
+        .fullScreenCover(isPresented: $isPresentedLoginView) {
+            LoginView()
+        }
         
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
